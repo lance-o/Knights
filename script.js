7b82e119667b1a72ef81858647c9b251d781edf9
Lawrence Cilia <cilialawrence@yahoo.co.uk>
2024-07-09 16:03:44 +0100

    fixed defense label being attack

--- script.js
+++ script.js
@@ -8,39 +8,39 @@ const maleniaDef = document.getElementById("MaleniaDef");
 const maleniaButton = document.getElementById("MaleniaButton");
 
 const radahn = {
-    hp: 50,
-    atk: 30,
-    def: 25,
-    attack: function(target){
-        target.hp -= this.atk - target.def;
-    }
-}
+  hp: 50,
+  atk: 30,
+  def: 25,
+  attack: function (target) {
+    target.hp -= this.atk - target.def;
+  },
+};
 
 const malenia = {
-    hp: 40,
-    atk: 45,
-    def: 15,
-    attack: function(target){
-        target.hp -= this.atk - target.def;
-    }
-}
+  hp: 40,
+  atk: 45,
+  def: 15,
+  attack: function (target) {
+    target.hp -= this.atk - target.def;
+  },
+};
 
 console.log(radahn.hp);
 
-radahnButton.addEventListener("click", function(target){
-    radahn.attack(malenia);
+radahnButton.addEventListener("click", function (target) {
+  radahn.attack(malenia);
 });
 
-maleniaButton.addEventListener("click", function(target){
-    malenia.attack(radahn);
+maleniaButton.addEventListener("click", function (target) {
+  malenia.attack(radahn);
 });
 
-window.requestAnimationFrame(function update(){
-    radahnHP.textContent=`HP: ${radahn.hp}`;
-    maleniaHP.textContent=`HP: ${malenia.hp}`;
-    radahnAtk.textContent=`Attack: ${radahn.atk}`;
-    maleniaAtk.textContent=`Attack: ${malenia.atk}`;
-    radahnDef.textContent=`Attack: ${radahn.def}`;
-    maleniaDef.textContent=`Attack: ${malenia.def}`;
-    window.requestAnimationFrame(update)
-})
+window.requestAnimationFrame(function update() {
+  radahnHP.textContent = `HP: ${radahn.hp}`;
+  maleniaHP.textContent = `HP: ${malenia.hp}`;
+  radahnAtk.textContent = `Attack: ${radahn.atk}`;
+  maleniaAtk.textContent = `Attack: ${malenia.atk}`;
+  radahnDef.textContent = `Defense: ${radahn.def}`;
+  maleniaDef.textContent = `Defense: ${malenia.def}`;
+  window.requestAnimationFrame(update);
+});
