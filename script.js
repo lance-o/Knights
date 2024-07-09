dc1db778ae13346e7d3a18e13ce06b5b0ff02ca7
Lawrence Cilia <cilialawrence@yahoo.co.uk>
2024-07-09 15:57:59 +0100

    added everything :)

---
+++ script.js
@@ -0,0 +1,46 @@
+const radahnHP = document.getElementById("RadahnHP");
+const radahnAtk = document.getElementById("RadahnAtk");
+const radahnDef = document.getElementById("RadahnDef");
+const radahnButton = document.getElementById("RadahnButton");
+const maleniaHP = document.getElementById("MaleniaHP");
+const maleniaAtk = document.getElementById("MaleniaAtk");
+const maleniaDef = document.getElementById("MaleniaDef");
+const maleniaButton = document.getElementById("MaleniaButton");
+
+const radahn = {
+    hp: 50,
+    atk: 30,
+    def: 25,
+    attack: function(target){
+        target.hp -= this.atk - target.def;
+    }
+}
+
+const malenia = {
+    hp: 40,
+    atk: 45,
+    def: 15,
+    attack: function(target){
+        target.hp -= this.atk - target.def;
+    }
+}
+
+console.log(radahn.hp);
+
+radahnButton.addEventListener("click", function(target){
+    radahn.attack(malenia);
+});
+
+maleniaButton.addEventListener("click", function(target){
+    malenia.attack(radahn);
+});
+
+window.requestAnimationFrame(function update(){
+    radahnHP.textContent=`HP: ${radahn.hp}`;
+    maleniaHP.textContent=`HP: ${malenia.hp}`;
+    radahnAtk.textContent=`Attack: ${radahn.atk}`;
+    maleniaAtk.textContent=`Attack: ${malenia.atk}`;
+    radahnDef.textContent=`Attack: ${radahn.def}`;
+    maleniaDef.textContent=`Attack: ${malenia.def}`;
+    window.requestAnimationFrame(update)
+})
